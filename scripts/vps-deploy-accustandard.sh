0335975790a7e8f7fe4b485c9de13c0cc8964779
JK <work@onekarlo.com>
2026-08-12 05:04:02 +0800

    chore: use floating LTS Alpine build tags

--- scripts/vps-deploy-accustandard.sh
+++ scripts/vps-deploy-accustandard.sh
@@ -20,7 +20,7 @@ podman run --rm --userns=keep-id \
   -v /workspace/node_modules \
   -v /workspace/.next \
   -w /workspace \
-  node:24-alpine \
+  node:lts-alpine \
   sh -lc 'npm ci && npm run build'
 test -f "${SOURCE_ROOT}/out/index.html"
 
